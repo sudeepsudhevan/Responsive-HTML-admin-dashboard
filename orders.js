77bbfd31e8270d05af7bc06fe5511d7c121c67fb
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-22 18:28:02 +0530

    Responsive html admin dashboard

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: "Product 1",
+        productPrice: 100,
+        paymentStatus: "Paid",
+        Status: "Delivered",
+    },
+    {
+        productName: "Product 2",
+        productPrice: 200,
+        paymentStatus: "Due",
+        Status: "Pending",
+    },
+    {
+        productName: "Product 3",
+        productPrice: 300,
+        paymentStatus: "Refunded",
+        Status: "Cancelled",
+    }
+];
